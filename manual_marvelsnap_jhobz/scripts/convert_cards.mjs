b8f0a16cae3493346ddca494496b4152955bb3ba
Jeff Hobson <jeffrey296@gmail.com>
2023-10-01 17:12:59 -0400

    Fix card import script writing to wrong file

--- manual_marvelsnap_jhobz/scripts/convert_cards.mjs
+++ manual_marvelsnap_jhobz/scripts/convert_cards.mjs
@@ -11,7 +11,7 @@ const convertCards = (from, to) => {
             progression: true
         }))
     try {
-        fs.writeFileSync('out.json', JSON.stringify(items))
+        fs.writeFileSync(to, JSON.stringify(items))
         console.log('Successfully converted cards')
     } catch (e) {
         console.error('Something went wrong', e)
@@ -20,4 +20,4 @@ const convertCards = (from, to) => {
     return 0
 }
 
-convertCards('../data/cards.json', 'out.json')
+convertCards('../data/cards.json', '../data/items.json')
