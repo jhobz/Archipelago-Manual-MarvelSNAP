fd838c3b93cc67cba9e28d0bda1c8e7755a74022
Jeff Hobson <jeffrey296@gmail.com>
2023-10-01 17:03:38 -0400

    Add September season cards
    Also adds energy cost categories to cards for easier filtering

--- manual_marvelsnap_jhobz/scripts/convert_cards.mjs
+++ manual_marvelsnap_jhobz/scripts/convert_cards.mjs
@@ -3,7 +3,13 @@ import fs from 'fs'
 const convertCards = (from, to) => {
     const infile = fs.readFileSync(from, 'utf8')
     const cards = JSON.parse(infile)
-    const items = cards.filter(({ collectible }) => collectible).map(({ name }) => ({ name, count: 1, category: ["Cards"], progression: true }))
+    const items = cards.filter(({ series, collectible }) => collectible && series).map(({ name, cost }) => (
+        {
+            name,
+            count: 1,
+            category: ["Cards", `${(cost <= 1 ? "0-1" : (cost >= 6 ? "6+" : cost))} Cost Cards`],
+            progression: true
+        }))
     try {
         fs.writeFileSync('out.json', JSON.stringify(items))
         console.log('Successfully converted cards')
